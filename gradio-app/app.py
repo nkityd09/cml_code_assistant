13d233e8d9d2d914f93b7639b4b19b4dff7faf80
nkityd09 <ankity@cloudera.com>
2023-09-27 22:37:57 -0700

    Create new Folder with Model File

--- gradio-app/app.py
+++ gradio-app/app.py
@@ -3,7 +3,7 @@ import os
 import gradio as gr
 import torch
 
-from model import get_input_token_length, run
+from codellama.model import get_input_token_length, run
 
 DEFAULT_SYSTEM_PROMPT = """\
 You are a helpful, respectful and honest assistant with a deep knowledge of code and software design. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\
